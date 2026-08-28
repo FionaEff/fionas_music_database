420c76da3c2e9fb44f94e8101afd2d6f7b8abf2b
FionaEff <137064797+FionaEff@users.noreply.github.com>
2026-08-28 10:59:31 +0200

    added index route, error templates and links to base.html

---
+++ app/main/routes.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sa
+from flask import render_template, flash, redirect, url_for
+from app import db
+from app.main import bp
+
+
+@bp.route("/", methods=["GET", "POST"])
+@bp.route("/index", methods=["GET", "POST"])
+def index():
+    return render_template("index.html", title="Home")
