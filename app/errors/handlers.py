3033209c78726f57eb9b3832c6abadaae1ac6ef0
FionaEff <137064797+FionaEff@users.noreply.github.com>
2026-09-22 09:31:38 +0200

    fixed typos in all views using forms, changed error handling for no discogs data

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
 from app import db
 from app.errors import bp
 from app.errors import exceptions
@@ -16,7 +16,10 @@ def internal_error(error):
     db.session.rollback()
     return render_template("errors/500.html"), 500
 
+
 @bp.app_errorhandler(exceptions.NoDataError)
 def no_data_error(error):
 
-    return render_template("errors/no_discogs_data.html")
+    flash("No Discogs Data found.")
+
+    return redirect(url_for("main.albums"))
